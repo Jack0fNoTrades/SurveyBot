c2b8386edc528143168aed5b44b59c1d6c43de78
86150 <a86150@MacBook-Air.local>
2023-09-16 15:14:34 -0400

    finish later

--- bot.py
+++ bot.py
@@ -1,7 +1,18 @@
 import pyautogui
-import keyboard
 import time
+import random
 
 
 def execute():
     pass
+
+
+print("Press q to exit")
+
+while True:
+    # do later
+    try:
+        execute()
+    # keyboard library would've been better but doesn't work on macOS
+    except KeyboardInterrupt:
+        break
