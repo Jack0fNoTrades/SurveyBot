7a678290564b7f068f590f08f8a49bf94eb575bf
86150 <a86150@MacBook-Air.local>
2023-11-11 21:04:17 -0500

    added new 2048 bot + required files and modulization

---
+++ util/util.py
@@ -0,0 +1,19 @@
+import random
+import pyautogui
+import time
+
+
+def click(x, y, x_factor_upper, y_factor_upper, x_factor_lower=0, y_factor_lower=0):
+    # The values of the first 4 parameters corresponds exactly to the attributes of a box object
+    # The last 2 parameters are only used if we don't want to start at the top left corner
+    x_randomizer = random.randint(x_factor_lower, x_factor_upper)
+    y_randomizer = random.randint(y_factor_lower, y_factor_upper)
+    x += x_randomizer
+    y += y_randomizer
+    # moveTo() avoids cursor teleporting and makes it more like human behavior.
+    pyautogui.moveTo(x, y, duration=0.2)
+    img = pyautogui.screenshot()
+    # perform the click
+    pyautogui.mouseDown(x, y)
+    time.sleep(0.3)
+    pyautogui.mouseUp(x, y)
