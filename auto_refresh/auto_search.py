4da81f48c7db1abb6bb7dcde1d71c9668acc0c91
Jack0fNoTrades <wang92@mcmaster.ca>
2023-11-24 00:03:57 -0500

    addressed the need to respond to events at the end of each refresh cycle

--- auto_refresh/auto_search.py
+++ auto_refresh/auto_search.py
@@ -1,12 +1,16 @@
 import pyautogui
 import time
 import random
+from util.util import click
+
+chrome = [540, 500]
+safari = [540, 465]
 
 
 # Although there are numerous front end applications that does this, they are not able to detect
 # events that are happening on screen, and some of them may not even have the option of randomized
 # refresh intervals.
-def auto_refresh():
+def auto_search(low, high, browser):
     while pyautogui.locateOnScreen("search_hit.png", grayscale=True, confidence=0.8) is None:
         pyautogui.keyDown("command")
         pyautogui.keyDown("r")
@@ -14,7 +18,8 @@ def auto_refresh():
         pyautogui.keyUp("command")
         pyautogui.keyUp("r")
         # wait a bit between searches
-        time.sleep(random.randint(180, 600))
+        time.sleep(random.randint(low, high))
+    click(browser[0], browser[1], 5, 5)
 
 
-auto_refresh()
+auto_search(5, 8, chrome)
