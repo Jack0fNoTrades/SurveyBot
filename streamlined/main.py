2da42e531ec669215c54c5ed6fc1692b0c408645
Jack0fNoTrades <wang92@mcmaster.ca>
2023-12-09 03:11:40 -0500

    optimized file/directory naming and execution

---
+++ streamlined/main.py
@@ -0,0 +1,13 @@
+from auto_search.auto_search import auto_search, chrome, safari
+from game_bot_2048.bot import execute
+from util.util import click
+
+# Integrates previously implemented scripts so that we can automate multiple tasks with only 1 execution.
+# Especially helpful for AFKing overnight.
+
+click(180, 60, 2, 2)
+execute()
+click(425, 60, 2, 2)
+auto_search(3, 5, chrome)
+click(340, 1065, 15, 15)
+auto_search(7, 10, safari)
