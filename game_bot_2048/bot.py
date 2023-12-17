16a1189b597165e4e3590b507a72d296288ac1c0
Jack0fNoTrades <wang92@mcmaster.ca>
2023-12-17 13:45:27 -0500

    implemented page reloading on execution

--- game_bot_2048/bot.py
+++ game_bot_2048/bot.py
@@ -1,7 +1,7 @@
 import time
 import pyautogui
 import random
-from util.util import click
+from util.util import click, refresh, print_position
 
 movement_list = ["left", "right", "up", "down"]
 desired_x_start_1 = 740
@@ -27,15 +27,26 @@ def play2048():
         pyautogui.press(movement_list[choice])
 
 
+def reload():
+    refresh()
+    # (510, 620) are the coordinates of the button
+    click(510, 620, 10, 10)
+    pyautogui.scroll(-5)
+
+
 # sleep for 3 seconds to allow time to manually bring game window in focus
 def execute():
-    time.sleep(3)
+    time.sleep(4)
+    reload()
+    # wait for loading to finish
+    while(pyautogui.locateOnScreen("playbutton.png", grayscale=True, confidence=0.8)) is None:
+        time.sleep(1)
     counter = 0
     # we only need 3 successful games per run
     while counter < 3:
         play2048()
         # detect if the most recent game is successful
-        if pyautogui.locateOnScreen("2SB.png", confidence=0.8) is not None:
+        if pyautogui.locateOnScreen("2SB.png", grayscale=True, confidence=0.8) is not None:
             counter += 1
         # reload game
         click(desired_x_start_2, desired_y_start_2, x_factor_2, y_factor_2)
@@ -44,6 +55,6 @@ def execute():
         time.sleep(sleep_time)
 
 
-# execute()
+execute()
 
 
