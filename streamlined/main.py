fc1792065ad350035f3cad863e83891467daf830
Jack0fNoTrades <wang92@mcmaster.ca>
2023-12-17 13:29:05 -0500

    implement custom scheduling

--- streamlined/main.py
+++ streamlined/main.py
@@ -1,3 +1,6 @@
+import time
+from datetime import datetime
+
 from auto_search.auto_search import auto_search, chrome, safari
 from game_bot_2048.bot import execute
 from util.util import click
@@ -6,10 +9,27 @@ from util.util import click
 # Especially helpful for AFKing overnight.
 
 # Set Safari browser icon position
-icon = [320, 1065]
+icon = [310, 1065]
 
-execute()
-click(425, 60, 2, 2)
-auto_search(7, 10, chrome)
-click(icon[0], icon[1], 15, 15)
-auto_search(3, 5, safari)
+
+def main():
+    execute()
+    click(425, 60, 2, 2)
+    auto_search(7, 10, chrome)
+    click(icon[0], icon[1], 15, 15)
+    auto_search(3, 5, safari)
+
+
+# The schedule library runs the program immediately when it is not supposed to,
+# and solutions are not available. Furthermore, our needs differ slightly from
+# simple periodic execution. Therefore, we write our own scheduler.
+def get_hour():
+    current_time = str(datetime.now().strftime("%H:%M:%S"))
+    return int(current_time[0:2])
+
+
+hour = get_hour()
+while hour < 4:
+    time.sleep(60 * 60)
+    hour = get_hour()
+main()
