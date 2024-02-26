b7ad2d8564c8aad0f82203d81ed999055954bc37
Jack0fNoTrades <wang92@mcmaster.ca>
2024-02-25 19:40:51 -0500

    added 3rd search & minor improvements to code and comments

--- streamlined/main.py
+++ streamlined/main.py
@@ -9,20 +9,29 @@ from util.util import click
 # Especially helpful for AFKing overnight.
 
 # Set Safari browser icon position
-icon = [290, 1065]
+safari_icon = [290, 1065]
+chrome_icon = [250, 1065]
 scheduled_hour = 4
 
 
 def main():
     execute()
     click(425, 60, 2, 2)
+    time.sleep(0.2)
     auto_search(7, 10, chrome)
-    click(icon[0], icon[1], 3, 3)
+    time.sleep(0.25)
+    click(safari_icon[0], safari_icon[1], 3, 3)
+    time.sleep(0.2)
     auto_search(3, 5, safari)
+    time.sleep(1)
+    click(chrome_icon[0], chrome_icon[1], 3, 3)
+    time.sleep(0.7)
+    click(1230, 180, 5, 5)
+    time.sleep(0.4)
+    auto_search(3, 5, chrome)
 
 
-# The schedule library runs the program immediately when it is not supposed to,
-# and solutions are not available. Furthermore, our needs differ slightly from
+# The schedule library runs the program immediately and our needs differ slightly from
 # simple periodic execution. Therefore, we write our own scheduler.
 def get_hour():
     current_time = str(datetime.now().strftime("%H:%M:%S"))
